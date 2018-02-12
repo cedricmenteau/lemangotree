f0c588eb42e139b3fbbc61fc8ccd58a26ecd0134
cedricmenteau <me.cedric@gmail.com>
2018-02-13 00:04:22 +0100

    Design reservation page

--- js/app.js
+++ js/app.js
@@ -65,12 +65,4 @@ var userFeed = new Instafeed({
 userFeed.run();
 
 
-$('#booking_form').parsley().on('field:validated', function() {
-    var ok = $('.parsley-error').length === 0;
-    $('.bs-callout-info').toggleClass('hidden', !ok);
-    $('.bs-callout-warning').toggleClass('hidden', ok);
-  })
-  .on('form:submit', function() {
-    return false; // Don't submit form for this demo
-  });
-});
+
